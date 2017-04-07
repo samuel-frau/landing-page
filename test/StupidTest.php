34b80174403064973e0d0f927ef8cef7456bb6f9
samuel-frau <samuelfrau@hotmail.it>
2017-04-07 10:04:27 +0200

    Changed StupidTest.php

--- test/StupidTest.php
+++ test/StupidTest.php
@@ -2,9 +2,13 @@
 namespace phpUnitTutorial\Test;
 class StupidTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTrueIsTrue()
-    {
+    public function testTrueIsTrue(){
         $foo = true;
         $this->assertTrue($foo);
+    }
+    public function testHtml(){
+    	$htmlOutput = '<p id="my_id"';
+    	$matcher = array('id' => 'my_id' );
+    	$test->assertTag($matcher, $htmlOutput);
     }
 }
