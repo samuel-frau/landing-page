781625079616a7e11420336766f6860c7ad2216d
samuel-frau <samuelfrau@hotmail.it>
2017-04-07 10:11:50 +0200

    Changed StupidTest.php

--- test/StupidTest.php
+++ test/StupidTest.php
@@ -7,7 +7,7 @@ class StupidTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($foo);
     }
     public function testHtml(){
-    	$htmlOutput = '<p id="my_id"';
+    	$htmlOutput = '<p id="my_id">Ciao</p>';
     	$matcher = array('id' => 'my_id' );
     	$test->assertTag($matcher, $htmlOutput);
     }
